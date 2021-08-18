1794befb0a8c71cf9e025c76772ff551305ec424
Christopher Crouzet <christopher@crouzet.pm>
2021-08-18 21:55:12 +1200

    Implement a custom assert

---
+++ src/base/assert.c
@@ -0,0 +1,45 @@
+#include "assert.h"
+
+#include <uuki/base/macros.h>
+#include <uuki/base/platform.h>
+
+#define WP_ASSERT_ASSERTION_FAILURE_FMT                                        \
+    "---------------------------\n"                                            \
+    " /!\\ ASSERTION FAILURE /!\\ \n"                                          \
+    "---------------------------\n"                                            \
+    "expression : %s\n"                                                        \
+    "  function : %s\n"                                                        \
+    "      file : %s\n"                                                        \
+    "      line : %d\n"
+
+// Public API
+// ---------------------------------------------------------------- //   O-(''Q)
+
+void
+w_handle_failed_assertion(
+    const char *expr,
+    const char *fn,
+    const char *file,
+    int line
+)
+{
+    char msg[1024];
+
+    if (
+        w_format_msg(
+            msg,
+            W_GET_ARRAY_LEN(msg),
+            WP_ASSERT_ASSERTION_FAILURE_FMT,
+            expr,
+            fn,
+            file,
+            line
+        )
+        == 0
+    )
+    {
+        w_print(W_STD_ERR, msg);
+    }
+
+    w_abort(-1);
+}
