f4c79044e865a2d45156520e4a5d9ca97ea980df
Christopher Crouzet <christopher@crouzet.pm>
2021-09-05 18:53:51 +1200

    Test against the status enum

--- tests/base/fs.c
+++ tests/base/fs.c
@@ -1,6 +1,7 @@
 #include <rexo.h>
 
 #include <uuki/base/fs.h>
+#include <uuki/base/status.h>
 
 #include <stddef.h>
 
@@ -10,18 +11,26 @@
 
 RX_TEST_CASE(fs, main)
 {
+    enum w_status status;
     struct w_file file;
     size_t size;
     char buf[SIZE + 1];
 
     buf[SIZE] = '\0';
 
-    RX_INT_REQUIRE_EQUAL(w_file_open(&file, FILE_PATH), 0);
-    RX_INT_REQUIRE_EQUAL(w_file_read(&file, NULL, &size), 0);
+    status = w_file_open(&file, FILE_PATH);
+    RX_INT_REQUIRE_EQUAL(status, W_SUCCESS);
+
+    status = w_file_read(&file, NULL, &size);
+    RX_INT_REQUIRE_EQUAL(status, W_SUCCESS);
     RX_UINT_REQUIRE_EQUAL(size, SIZE);
-    RX_INT_REQUIRE_EQUAL(w_file_read(&file, buf, &size), 0);
+
+    status = w_file_read(&file, buf, &size);
+    RX_INT_REQUIRE_EQUAL(status, W_SUCCESS);
     RX_STR_REQUIRE_EQUAL(buf, BUF);
-    RX_INT_REQUIRE_EQUAL(w_file_close(&file), 0);
+
+    status = w_file_close(&file);
+    RX_INT_REQUIRE_EQUAL(status, W_SUCCESS);
 }
 
 int
